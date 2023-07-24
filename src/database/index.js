1c515cd8d887e9a4bcc9fd8d4c104869814e3d81
PedroNogueir4 <pedroffn209@gmail.com>
2023-07-24 16:40:50 -0300

    Conexão com Mongo realizada

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from "sequelize"
+import mongoose from "mongoose"
 import configDataBase from "../config/database"
 
 import User from "../app/models/User"
@@ -9,11 +10,22 @@ const models = [User, Haircut]
 class Database {
   constructor() {
     this.init()
+    this.mongo()
   }
 
   init() {
     this.connection = new Sequelize(configDataBase)
     models.map((model) => model.init(this.connection))
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      "mongodb://localhost:27017/barberapp",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )
   }
 }
 export default new Database()
