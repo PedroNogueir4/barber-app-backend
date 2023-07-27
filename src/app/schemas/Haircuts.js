b2d0af0ed8dee99e33f5cb21784bd629a9030ad8
PedroNogueir4 <pedroffn209@gmail.com>
2023-07-27 17:46:09 -0300

    Schema de Haircuts implementado

---
+++ src/app/schemas/Haircuts.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose"
+
+const Haircut = new Schema(
+  {
+    user: {
+      id: {
+        type: Number,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+    haircut: [
+      {
+        id: {
+          type: Number,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+export default mongoose.model("Corte", Haircut)
